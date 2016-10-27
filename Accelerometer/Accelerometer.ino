95e7245ca3b72202cd12401a4414e8e9eb1a7e87
BumMo Koo <ksquareatm@gmail.com>
2016-10-27 14:33:26 +0900

    Added flicker when starting; Changed light to analog pins; Changed variable names.

--- Accelerometer/Accelerometer.ino
+++ Accelerometer/Accelerometer.ino
@@ -5,20 +5,21 @@
 */
 
 // these constants describe the pins. They won't change:
-const int pinGround = 18;             // analog input pin 4 -- ground
-const int pinPower = 19;              // analog input pin 5 -- voltage
-const int pinX = A3;                  // x-axis of the accelerometer
-const int pinY = A2;                  // y-axis
-const int pinZ = A1;                  // z-axis (only on 3-axis models)
+const int pinGround = 18; 
+const int pinPower = 19;  
+const int pinX = A3;      
+const int pinY = A2;      
+const int pinZ = A1;      
 
-const int pinWhite = 13;
-const int pinBlue = 12;
+const int pinWhite = 10;
+const int pinBlue = 9;
 
 // Settings
 int calibrationX = 0;
 int calibrationY = 0;
 int calibrationZ = 0;
-int offset = 50;
+int calibrationOffset = 50;
+int fadeDelay = 2;
 
 // Values
 int accelerationX = 0;
@@ -56,6 +57,27 @@ void readAcceleration() {
   loopCount++;
 }
 
+void lightFadeIn(int pin) {
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(pin, i);
+    delay(fadeDelay);
+  }
+}
+
+void lightFadeOut(int pin) {
+  for (int i = 255; i >=0; i--) {
+    analogWrite(pin, i);
+    delay(fadeDelay);
+  }
+}
+
+void lightFlicker(int count) {
+  for (int i = 0; i < count; i++) {
+    lightFadeIn(pinWhite);
+    lightFadeOut(pinWhite);
+  }
+}
+
 void setup() {
   // Setup
   Serial.begin(9600); // Start communication
@@ -75,7 +97,7 @@ void setup() {
   logHeader();
 
   // Ready
-  // TODO: Flicker twice
+  lightFlicker(2);
   delay(5000);
 }
 
@@ -83,12 +105,12 @@ void loop() {
   readAcceleration();
   logAcceleration();
 
-  if (accelerationX < calibrationX - offset ||
-    accelerationX > calibrationX + offset ||
-    accelerationY < calibrationY - offset ||
-    accelerationY > calibrationY + offset ||
-    accelerationZ < calibrationZ - offset ||
-    accelerationZ > calibrationZ + offset) {
+  if (accelerationX < calibrationX - calibrationOffset ||
+    accelerationX > calibrationX + calibrationOffset ||
+    accelerationY < calibrationY - calibrationOffset ||
+    accelerationY > calibrationY + calibrationOffset ||
+    accelerationZ < calibrationZ - calibrationOffset ||
+    accelerationZ > calibrationZ + calibrationOffset) {
 
     if (loopCount < 100) {
       Serial.println("BLUE");
